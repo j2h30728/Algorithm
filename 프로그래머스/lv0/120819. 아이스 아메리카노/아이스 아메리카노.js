0ccd5246b463a29f52eae9c4d2ace6e4cd512599
j2h30728 <60846068+j2h30728@users.noreply.github.com>
2023-02-12 01:42:36 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,6 @@
 function solution(money) {
    const americano =  Math.floor(money/5500)
-   const remain = money - americano * 5500
+   const remain = money%5500
     var answer = [americano, remain];
     return answer;
 }
