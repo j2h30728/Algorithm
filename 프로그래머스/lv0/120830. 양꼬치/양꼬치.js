84b36913dac912d7bf9d2c055380fc60c044db2b
j2h30728 <60846068+j2h30728@users.noreply.github.com>
2023-02-13 22:12:00 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.js
@@ -1,7 +1,4 @@
 function solution(n, k) {
     const service = Math.floor(n/10)
-   if (service > 0 && k - service >= 0){
-       return n * 12000 +( k -service) * 2000
-   }
-    return n*12000 + k * 2000
+    return n*12000 + (k-service) * 2000
 }
