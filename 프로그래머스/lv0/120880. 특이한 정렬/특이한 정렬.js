9a33a748f6f9b340f296e9817108e7124a121ebd
j2h30728 <60846068+j2h30728@users.noreply.github.com>
2023-04-24 09:52:35 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,9 @@
+function solution(numlist, n) {
+    const arr = numlist.map(x=>[x, Math.abs(n-x)])
+    return arr.sort((a,b)=> {
+        if(a[1] < b[1]) return -1
+        if(a[1] === b[1]){
+            return a[0] < b[0] ? 1 : -1;
+        }
+    }).map(x=> x[0])
+}
