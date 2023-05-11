a9397978321c6bd219adb5a0c060e65a0272f19e
j2h30728 <60846068+j2h30728@users.noreply.github.com>
2023-05-12 08:23:58 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,9 +1,13 @@
 function solution(n) {
-    let num = 0;
-    let answer = ""
-    while(num < n){
-        answer += (num % 2 === 0) ? "수" : "박";
-        num++;
-    }
-    return answer;
+    return `${"수박".repeat(Math.floor(n/2))}${(n % 2 === 1) ? "수" : ""}`
 }
+
+// function solution(n) {
+//     let num = 0;
+//     let answer = ""
+//     while(num < n){
+//         answer += (num % 2 === 0) ? "수" : "박";
+//         num++;
+//     }
+//     return answer;
+// }
