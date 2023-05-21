aa8b48b13dbfb0182f430004fca7491c90ce7e0f
JiHyeon <60846068+j2h30728@users.noreply.github.com>
2023-05-22 08:40:08 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,10 +1,6 @@
 function solution(common) {
-    if(common.length === 2) return common[1] - common[0];
-    // const end =common[common.length - 1];
-    // const start = common[0];
-    const endIdx =common.length-1;
-    if(common[1]- common[0] === common[endIdx] - common[endIdx-1]) {
-        return common[1] - common[0] + common[endIdx]
+    if(common[1] - common[0] === common[2] - common[1]) {
+        return common[1] - common[0] + common.pop();
     }
-    return common[1] / common[0] * common[endIdx]
+    return common[1] / common[0] * common.pop();
 }
