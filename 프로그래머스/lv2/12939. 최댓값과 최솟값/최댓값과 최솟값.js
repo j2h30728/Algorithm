6f8ebdc809dbb49dde4043a1069c3f74b73fd08c
Dam <60846068+j2h30728@users.noreply.github.com>
2023-05-29 08:40:19 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,10 @@
 function solution(s) {
-    var answer = '';
-    return answer;
+    const numArr = s.split(' ').map(x=>parseInt(x));
+    return `${Math.min(...numArr)} ${Math.max(...numArr)}`
 }
+/*
+function solution(s) {
+    const numArr = s.split(' ').map(x=>parseInt(x)).sort((a,b)=>a-b)
+    return `${numArr[0]} ${numArr[numArr.length - 1]}`
+}
+*/
