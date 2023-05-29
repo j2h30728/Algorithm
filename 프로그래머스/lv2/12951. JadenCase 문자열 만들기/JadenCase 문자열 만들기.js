84d3da5a456b8faa0ddb0c264ec7bb8ae861fc33
Dam <60846068+j2h30728@users.noreply.github.com>
2023-05-30 08:34:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -4,14 +4,3 @@ function solution(s) {
         return x[0].toUpperCase() + x?.slice(1).toLowerCase()
     }).join(' ')
 }
-/*
-function solution(s) {
-    return s.split(' ').map(x=> {
-        const newStr = x.slice(1).toLowerCase();
-        if(!!isNaN(parseInt(x[0]))){
-            return x[0].toUpperCase() + newStr;
-        }
-        return x[0] + newStr;
-    }).join(' ');
-}
-*/
