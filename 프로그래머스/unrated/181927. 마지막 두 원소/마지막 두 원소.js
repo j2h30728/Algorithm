92579e7da6de8ffc79365543461f1c3d2ed60751
Dam <60846068+j2h30728@users.noreply.github.com>
2023-06-08 08:55:13 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,13 +1,12 @@
 function solution(num_list) {
-    const length = num_list.length;
-    if(num_list[length-1] > num_list[length-2]){
-        return [...num_list , num_list[length-1] -num_list[length-2]];
-    }else{
-        return [...num_list, num_list[length-1]*2];
-    }
+    const [a,b] = [...num_list].reverse();
+    return [...num_list, (a>b)? a-b : a *2];
 }
-//마지막 원소가 그전 원소보다 크면
-//마지막 원소에서 그전 원소를 뺀 값
-
-//마지막 원소가 그전 원소보다 크지 않다면 마
-//지막 원소를 두 배한 값을 추가하여 return
+// function solution(num_list) {
+//     const length = num_list.length;
+//     if(num_list[length-1] > num_list[length-2]){
+//         return [...num_list , num_list[length-1] -num_list[length-2]];
+//     }else{
+//         return [...num_list, num_list[length-1]*2];
+//     }
+// }
