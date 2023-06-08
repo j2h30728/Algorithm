a1c005848ebbca43cf44bb894e75b8ad9373ea6d
Dam <60846068+j2h30728@users.noreply.github.com>
2023-06-09 08:34:45 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 0.11 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -2,10 +2,16 @@ function solution(myString, pat) {
     if(myString.length < pat.length) return 0;
     const newStr = myString.toLowerCase();
     const newPat = pat.toLowerCase();
-    for(let i = 0; i <= newStr.length - newPat.length ; i++){
-        if(newStr.slice(i,newPat.length + i) === newPat){
-            return 1;
-        }
-    }
-    return 0;
+    return newStr.includes(newPat) ? 1 : 0;
 }
+// function solution(myString, pat) {
+//     if(myString.length < pat.length) return 0;
+//     const newStr = myString.toLowerCase();
+//     const newPat = pat.toLowerCase();
+//     for(let i = 0; i <= newStr.length - newPat.length ; i++){
+//         if(newStr.slice(i,newPat.length + i) === newPat){
+//             return 1;
+//         }
+//     }
+//     return 0;
+// }
