3cd42f8e50fbc6c130cac6e3270b971d10a3b3f9
Dam <60846068+j2h30728@users.noreply.github.com>
2023-06-28 08:18:53 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,12 @@
+function solution(cards1, cards2, goal) {
+    for(const word of goal){
+        if(cards1[0] === word){
+            cards1.shift(1);
+        }else if(cards2[0] === word){
+            cards2.shift(1);
+        }else{
+            return "No";
+        }
+    }
+    return "Yes";
+}
