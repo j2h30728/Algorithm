a574e6b7f259e5d352c9a3f8b4e97845689c5413
Dam <60846068+j2h30728@users.noreply.github.com>
2023-07-01 09:11:31 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.05 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -1,11 +1,11 @@
 function solution(arr) {
-    const max = Math.max(arr.length,arr[1].length)
+    const max = Math.max(arr.length,arr[0].length)
     if(max > arr.length){
         while(max > arr.length){
             arr.push(Array.from({length:max}).fill(0));
         }
     }
-    if(max > arr[1].length){
+    if(max > arr[0].length){
         for(let i=0; i< arr.length ; i++){
             while(max > arr[i].length){
              arr[i].push(0);  
