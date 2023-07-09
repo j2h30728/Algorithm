d7afa2ce03131f78c5f9102808771346bbd4a2a3
Dam <60846068+j2h30728@users.noreply.github.com>
2023-07-10 08:24:35 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,4 +1,5 @@
 function solution(lottos, win_nums) {
+    const rank = [6,6,5,4,3,2,1]
     let zeroCount =0;
     const filteredArr = lottos.filter(x=> {
         if(x===0){
@@ -7,23 +8,23 @@ function solution(lottos, win_nums) {
         return win_nums.includes(x)
     })
     const matchNum =  filteredArr.length;
-    return [makeRanking(filteredArr.length+zeroCount), makeRanking(filteredArr.length)]
+    return [rank.at(filteredArr.length+zeroCount), rank.at(filteredArr.length)]
 }
 
 
-function makeRanking(num) {
-    switch(num){
-            case(6):
-            return 1;
-            case(5):
-            return 2;
-            case(4):
-            return 3;
-            case(3):
-            return 4;
-            case(2):
-            return 5;
-        default:
-            return 6;
-    }
-}
+// function makeRanking(num) {
+//     switch(num){
+//             case(6):
+//             return 1;
+//             case(5):
+//             return 2;
+//             case(4):
+//             return 3;
+//             case(3):
+//             return 4;
+//             case(2):
+//             return 5;
+//         default:
+//             return 6;
+//     }
+// }
