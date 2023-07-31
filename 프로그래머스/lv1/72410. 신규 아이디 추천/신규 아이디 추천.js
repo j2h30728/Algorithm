b04f6d05c4ab7bc2d3b8b50a53737d54b4519751
Dam <60846068+j2h30728@users.noreply.github.com>
2023-07-31 16:16:33 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,26 +1,14 @@
 function solution(new_id) {
-    const newId2 =new_id.toLowerCase().split('').filter(x=> /^[a-z0-9._-]+$/.test(x) );
+     new_id =new_id.toLowerCase().replace(/[^\w-_.]/g, '').replace(/\.{2,}/g,'.').replace(/^\.|\.$/g,'');
     
-    const newId3 = [];
-    for(const ch of newId2){
-        if(newId3.at(-1) === "." && ch === ".") continue;
-        newId3.push(ch);
-    }
-    if(newId3.at(0) === "."){
-        newId3.splice(0,1);
-    }
-    if(newId3.at(-1) === "."){
-        newId3.splice(newId3.length-1);
-    }
-    const newId5 = newId3.length === 0 ? ['a'] : newId3;
+    new_id = new_id.length === 0 ? 'a' : new_id;
     
-    const newId6 = newId5.slice(0, 15)
-    if(newId6.at(-1) === ".") newId6.splice(newId6.length-1);
+    new_id= new_id.length >= 16 ? new_id.slice(0, 15).replace(/\.$/g,'') : new_id;
     
-    if(newId6.length <= 2){
-        while(newId6.length < 3){
-            newId6.push(newId6.at(-1));
+    if(new_id.length <= 2){
+        while(new_id.length < 3){
+            new_id += new_id.at(-1);
         }
     }
-    return newId6.join('');
+    return new_id;
 }
