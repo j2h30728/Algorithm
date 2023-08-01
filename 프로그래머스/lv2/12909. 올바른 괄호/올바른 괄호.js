671853d862a562967bb4b1f9cf81e5242aef967a
Dam <60846068+j2h30728@users.noreply.github.com>
2023-08-01 10:28:28 +0900

    [level 2] Title: 올바른 괄호, Time: 6.83 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,5 +1,5 @@
 function solution(s){
-    if(s.length % 2 ===1  || !s.includes("(") || !s.includes(")") ) return false;
+    if(s.length % 2 ===1 ) return false;
     const arr = [];
     for(let i of s){
         if(i ===")"){
@@ -13,24 +13,5 @@ function solution(s){
             arr.push(i);
         }
     }
-    return arr.length === 0 ? true : false;
+    return !arr.length;
 }
-/*
-function solution(s){
-    if(s.length === 0) return false;
-    let startCount = 0;
-    for(let i =0; i < s.length ; i++){
-     console.log(s.substring(i,i+1))   
-        if(s.substring(i,i+1) === "("){
-            startCount++;
-        }
-        if(s.substring(i,i+1) === ")"){
-            if(startCount > 0){
-                startCount--;
-            }else {
-                return false;
-            }
-        }
-    }
-    return startCount === 0 ? true : false;
-*/
