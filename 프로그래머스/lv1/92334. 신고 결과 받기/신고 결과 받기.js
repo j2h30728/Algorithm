6a1ca3e3d3e32e7cfbdae8ec4336b5276d4f38f1
Dam <60846068+j2h30728@users.noreply.github.com>
2023-08-16 14:00:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,23 +1,23 @@
 function solution(id_list, report, k) {
-    const user = new Map();
-    const 신고카운트 = new Map();
+    const 사용자 = new Map();
+    const 신고횟수 = new Map();
     const 정지 = Array.from({length : id_list.length}).fill(0);
     report.forEach(x=>{
         const [신고자, 피신고자] = x.split(' ');
-        if(user.get(신고자)){
-            const 피신고자들 = user.get(신고자);
+        if(사용자.get(신고자)){
+            const 피신고자들 = 사용자.get(신고자);
             if(!피신고자들.includes(피신고자)) 피신고자들.push(피신고자);
         } else{
-            user.set(신고자,[피신고자])
+            사용자.set(신고자,[피신고자])
         }
     });
-    Array.from(user.values()).flat().forEach(신고된ID => {
-        신고카운트.get(신고된ID) ? 신고카운트.set(신고된ID, 신고카운트.get(신고된ID) + 1) : 신고카운트.set(신고된ID, 1) 
+    Array.from(사용자.values()).flat().forEach(신고된ID => {
+        신고횟수.get(신고된ID) ? 신고횟수.set(신고된ID, 신고횟수.get(신고된ID) + 1) : 신고횟수.set(신고된ID, 1) 
     })
 
-    for(const [신고자, 피신고자들] of user.entries()){
+    for(const [신고자, 피신고자들] of 사용자.entries()){
         피신고자들.forEach(피신고자 => {
-            if(신고카운트.get(피신고자) >= k) 정지[id_list.indexOf(신고자)] += 1;
+            if(신고횟수.get(피신고자) >= k) 정지[id_list.indexOf(신고자)] += 1;
         })
     }
     return 정지;
