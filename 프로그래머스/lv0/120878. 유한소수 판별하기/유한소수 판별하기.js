d74e9105e0f82095ba49e9837175836449b9de90
Dam <60846068+j2h30728@users.noreply.github.com>
2023-08-19 23:18:33 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,24 +1,30 @@
 function solution(a, b) {
-    // let x = a;
     let y= 0;
     for(let i =1; i <= Math.min(a,b); i++){
-        if(a%i ===0 && b%i ===0 ) {
-            // x = x/i;
-            // y = y/i;
-            y=i;
-        }
+        if(a%i ===0 && b%i ===0 ) y=i;
     }
     let c = b/y;
-    let num = 2
-    const arr = [];
-    while(num <= c){
-        if(c % num === 0) {                        
-            c = c/num;
-            arr.push(num)
-
-        } else{
-            num++;
-        }
-    }
-    return arr.filter(x=> x !== 2 && x !== 5).length > 0 ? 2 : 1;
+    while(c%2===0){c = c/2}
+    while(c%5===0){c = c/5}
+    return c !== 1 ? 2 : 1; 
 }
+
+// function solution(a, b) {
+//     let y= 0;
+//     for(let i =1; i <= Math.min(a,b); i++){
+//         if(a%i ===0 && b%i ===0 ) y=i;
+//     }
+//     let c = b/y;
+//     let num = 2
+//     const arr = [];
+//     while(num <= c){
+//         if(c % num === 0) {                        
+//             c = c/num;
+//             arr.push(num)
+
+//         } else{
+//             num++;
+//         }
+//     }
+//     return arr.filter(x=> x !== 2 && x !== 5).length > 0 ? 2 : 1;
+// }
