7628bd523bfefe0586713ce5945597fd93d98692
Dam <60846068+j2h30728@users.noreply.github.com>
2023-08-23 22:26:07 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,4 +1,15 @@
 function solution(n, arr1, arr2) {
+    const result = [];
+
+    for(let i=0; i < n; i++){
+        const star = (arr1[i] | arr2[i]).toString(2).padStart(n,0).replaceAll(1,"#").replaceAll(0," ")
+        result.push(star)
+    }
+    return result;
+}
+
+/*
+function solution(n, arr1, arr2) {
     const newArr1 = arr1.map(x=>x.toString(2).padStart(n,0).split(''))
     const newArr2 = arr2.map(x=>x.toString(2).padStart(n,0).split(''));
     const result = Array.from({length :n}).fill([]);
@@ -15,3 +26,4 @@ function solution(n, arr1, arr2) {
     }
     return result;
 }
+*/
