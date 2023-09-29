11bb23e2e464756e125b251a606329c0fd41b44c
Dam <60846068+j2h30728@users.noreply.github.com>
2023-09-29 09:48:06 +0900

    [level 2] Title: 할인 행사, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.js
@@ -1,20 +1,23 @@
 function solution(want, number, discount) {
     const totalNumber = number.reduce((acc,curr)=> acc+curr,0)
     let count = 0;
-    const wantObj = {};
-    for(let j=0; j < want.length ; j++){
-        wantObj[want[j]] = number[j];
-    }
-    for(let i =0; i < discount.length -totalNumber+1;i++){
-        const newArr = discount.slice(i,totalNumber+i);
-        const newObj = {...wantObj};
-        for(let k=0; k < newArr.length; k++){
-            if(newObj[newArr[k]]){
-                newObj[newArr[k]] -= 1;
-                if(k === newArr.length-1) count++;
-            }else {
-                break;
+    
+    for(let i =0; i <= discount.length -totalNumber;i++){
+        let availableDays =0;
+        const wantObj = {};
+        for(let j=0; j < want.length ; j++){
+            wantObj[want[j]] = number[j];
+        }
+        for(let j =0; j < totalNumber; j++){
+            const product = discount[i+j];
+            if(wantObj[product]){
+                wantObj[product]--;
+                availableDays++;
+                if(wantObj[product] === 0) delete wantObj[product]
             }
+        }
+        if(Object.keys(wantObj) && availableDays === totalNumber){
+            count++;
         }
     }
     return count;
