d055412f6fcd24d6bf4cb9489d953e9b397c6a76
Dam <60846068+j2h30728@users.noreply.github.com>
2023-10-05 00:24:08 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split(" ");
+
+function solution(a, b) {
+  console.log(Number(a) / Number(b));
+}
+solution(input[0], input[1]);
