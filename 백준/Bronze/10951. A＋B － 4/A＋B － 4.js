47741417df685592be4ed7e7966ac4227738829d
Dam <60846068+j2h30728@users.noreply.github.com>
2023-10-09 01:25:43 +0900

    [Bronze V] Title: A+B - 4, Time: 116 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map((x) => x.split(" ").map((x) => parseInt(x)));
+
+function solution(input) {
+  const result = [];
+  for (const [a, b] of input) {
+    result.push(a + b);
+  }
+  return result.join("\n");
+}
+console.log(solution(input));
