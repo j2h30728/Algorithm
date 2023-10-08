0c4913f952a11ec7fe4f589ed777d4cec44f8c19
Dam <60846068+j2h30728@users.noreply.github.com>
2023-10-09 01:15:47 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")[0]
+  .split(" ")
+  .map((x) => parseInt(x));
+
+function solution(a, b) {
+  return [a + b, a - b, a * b, Math.floor(a / b), a % b].join("\n");
+}
+console.log(solution(input[0], input[1]));
