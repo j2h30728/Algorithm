a6992ef8cd82fbd350cfb1bbd14b1587f8f9ca80
Dam <60846068+j2h30728@users.noreply.github.com>
2023-10-13 09:25:46 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 128 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")[0]
+  .split(" ")
+  .map((x) => parseInt(x));
+
+function solution(a, b) {
+  let num1 = a;
+  let num2 = b;
+  while (num2 !== 0) {
+    let temp = num1 % num2;
+    num1 = num2;
+    num2 = temp;
+  }
+  const LCM = (a * b) / num1;
+  return [num1, LCM].join("\n");
+}
+console.log(solution(input[0], input[1]));
