a8b9a89cd9d0000d08c52a6fbb68530944807063
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-06 09:27:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 440 ms, Memory: 53268 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.js
@@ -1,15 +1,18 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [num, ...arr] = input;
+const [n, ...arr] = input;
 
 function solution(a, b) {
-  b.sort((a, b) => a[0] - b[0]);
-  return b.map((x) => x.join(" ")).join("\n");
+  return b
+    .sort((a, b) => a[0] - b[0])
+    .map((x) => x.join(" "))
+    .join("\n");
 }
+
 console.log(
   solution(
-    Number(num),
+    Number(n),
     arr.map((x) => x.split(" "))
   )
 );
