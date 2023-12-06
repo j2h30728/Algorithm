0b0be2d4173dbdb26374e00cdf7206e94f021fad
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-06 09:36:16 +0900

    [Silver V] Title: 소트인사이드, Time: 120 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -3,10 +3,15 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n")[0];
 
 function solution(a) {
-  return a
-    .split("")
-    .sort((a, b) => b - a)
-    .join("");
+  const count = Array.from({ length: 10 }, () => 0);
+  for (const n of a) {
+    count[n]++;
+  }
+  let result = "";
+  for (let i = 9; i >= 0; i--) {
+    result += `${i}`.repeat(count[i]);
+  }
+  return result;
 }
 
 console.log(solution(input));
