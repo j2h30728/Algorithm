948605f99ace26e248cdf746a5146354da022593
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-06 09:19:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1536 ms, Memory: 329452 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.js
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -1,18 +1,15 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
-const [a, arr] = input;
+const [n, arr] = input;
 
-const format = (str) => str.split(" ").map(Number);
-
-function solution(n, arr) {
-  const set = Array.from(new Set(arr).values()).sort((a, b) => a - b);
-
+function solution(a, b) {
   const map = new Map();
-  set.forEach((value, index) => {
-    map.set(value, index);
-  });
-  return arr.map((x) => map.get(x)).join(" ");
+  Array.from(new Set(b))
+    .sort((a, b) => a - b)
+    .forEach((x, i) => map.set(x, i));
+
+  return b.map((x) => map.get(x)).join(" ");
 }
 
-console.log(solution(Number(a), format(arr)));
+console.log(solution(Number(n), arr.split(" ").map(Number)));
