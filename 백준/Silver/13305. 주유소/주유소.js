e0d63351b583306adef6939baf2bd3c82989988f
Dam <60846068+j2h30728@users.noreply.github.com>
2023-12-19 09:44:18 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 36984 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,16 +3,18 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const [M, N, price] = input;
 
-function solution(x, y, z) {
-  const money = [];
-  let min = 999;
-  for (let i = 0; i < x - 1; i++) {
-    min = Math.min(z[i], min);
-    money.push(min);
+function solution(x, dist, cost) {
+  let minCost = cost[0];
+  for (let i = 0; i < x; i++) {
+    minCost = Math.min(cost[i], minCost);
+    cost[i] = minCost;
   }
-  const result = y.reduce((acc, cur, index) => acc + cur * money[index], 0);
+  let result = BigInt(0);
+  for (let i = 0; i < x - 1; i++) {
+    result += BigInt(dist[i]) * BigInt(cost[i]);
+  }
 
-  return result;
+  return String(result);
 }
 
 console.log(solution(Number(M), N.split(" ").map(Number), price.split(" ").map(Number)));
