6b3076af9824b2dab46863b35f7396da99ec33a1
Dam <60846068+j2h30728@users.noreply.github.com>
2024-01-14 09:06:36 +0900

    Time: 52 ms (74.08%), Space: 42.2 MB (90.67%) - LeetHub

--- LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -13,14 +13,21 @@
 var removeNthFromEnd = function (head, n) {
     let fast = head;
     let slow = head;
+
+    // fast vhdlsxjfmf n 노드 만큼 앞으로 이동
     for (let i = 0; i < n; i++) {
         fast = fast.next;
     }
+
+    // 제거할 노드가 첫번째 노드일 경우
     if (!fast) return head.next;
+
     while (fast.next) {
         slow = slow.next;
         fast = fast.next;
     }
+
+    //제거 할 노드
     slow.next = slow.next.next;
     return head;
 };
