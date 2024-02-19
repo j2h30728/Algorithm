2eb2e8288da2068560abdca5905f964034c7bb1a
Dam <60846068+j2h30728@users.noreply.github.com>
2024-02-19 22:36:21 +0900

    Time: 87 ms (15.97%), Space: 53.2 MB (25.62%) - LeetHub

---
+++ LeetCode/Easy/0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    if (n === 0 || n < 0) return false;
+    while (n > 1) {
+        n = n / 2;
+    }
+    return n === 1;
+};
