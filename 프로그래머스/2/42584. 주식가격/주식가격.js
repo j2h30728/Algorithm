42628c7c701a13c6f9eb9c845ed9656f51e6e11d
Dam <60846068+j2h30728@users.noreply.github.com>
2024-02-23 16:57:40 +0900

    [level 2] Title: 주식가격, Time: 5.27 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,11 +1,9 @@
 function solution(prices) {
-    const answer = [];
-    for(let i =0; i < prices.length; i++){
-        for(let j = i; j < prices.length; j++){
-            if(prices[i] > prices[j] || j === prices.length-1){
-                answer.push(j-i);
-                break;
-            }
+    var answer = new Array(prices.length).fill(0);
+    for(let i = 0; i < prices.length -1; i++){
+        for(let j = i + 1; j < prices.length; j++){
+            answer[i]++;
+            if(prices[i] > prices[j]) break;
         }
     }
     return answer;
