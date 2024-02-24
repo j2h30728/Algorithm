3eaade7410aafeba958a18e865bb6257f3cc12c7
Dam <60846068+j2h30728@users.noreply.github.com>
2024-02-24 18:54:07 +0900

    [level 2] Title: 소수 찾기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -1,24 +1,32 @@
-function isPrime(num) {
-    if (num < 2) return false;
-    for (let i = 2; i * i <= num; i++) {
-        if (num % i === 0) return false;
+const isPrime = (num) => {
+    if(num < 2 ) return false;
+    for(let i = 2; i <= Math.sqrt(num); i++){
+        if(num % i === 0) return false;
     }
     return true;
 }
 
 function solution(numbers) {
-    const numSet = new Set();
+    const set = new Set();
+    const used = new Array(numbers.length).fill(false);
     
-    const makeCombination = (acc, nums) => {
-        if (acc.length > 0) numSet.add(parseInt(acc));
-        if (nums.length === 0) return;
-
-        nums.forEach((num, index) => {
-            const newNums = nums.slice(0, index).concat(nums.slice(index + 1));
-            makeCombination(acc + num, newNums);
-        });
-    };
-
-    makeCombination("", numbers.split(""));
-    return Array.from(numSet).filter(isPrime).length;
+    function dfs (current) {
+        if(current.length > 0 && isPrime(Number(current))){
+            set.add(Number(current));
+        }
+        
+        for (let i = 0; i < numbers.length; i++){
+            if(!used[i]){
+                used[i] = true;
+                dfs(current + numbers[i]);
+                used[i] = false;
+            }
+        }
+    }
+    
+    dfs('');
+    return set.size;
 }
+
+
+
