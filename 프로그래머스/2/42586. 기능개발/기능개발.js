79b7081f9d552a75b6b40d9706781a84446d0c52
Dam <60846068+j2h30728@users.noreply.github.com>
2024-02-24 09:54:28 +0900

    [level 2] Title: 기능개발, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,4 +1,17 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    return answer;
+    const days = progresses.map((progress,i)=> Math.ceil((100 - progress) / speeds[i]));
+    const result = [];    
+    let maxDay = days[0]
+    let count = 1;
+    for(let i = 1; i < days.length; i++){
+        if(days[i] <= maxDay ){
+            count++;
+        }else{
+            maxDay = days[i];
+            result.push(count);
+            count = 1;
+        }
+    }
+    result.push(count);
+    return result;
 }
