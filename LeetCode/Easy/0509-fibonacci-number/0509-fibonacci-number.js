32ff47398d2840a793f4f6ffa9ee3b9a5687546d
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-03 09:14:10 +0900

    Time: 43 ms (95.44%), Space: 48.3 MB (59.54%) - LeetHub

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.js
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,7 +3,16 @@
  * @return {number}
  */
 var fib = function (n) {
-    if (n <= 0) return 0;
-    if (n === 1) return 1;
-    return fib(n - 1) + fib(n - 2);
+    const memo = {};
+
+    function helper(n) {
+        if (n <= 0) return 0;
+        if (n === 1) return 1;
+        if(memo[n]) return memo[n];
+
+        const result = helper(n - 1) + helper(n - 2);
+        memo[n] = result;
+        return result;
+    }
+    return helper(n);
 };
