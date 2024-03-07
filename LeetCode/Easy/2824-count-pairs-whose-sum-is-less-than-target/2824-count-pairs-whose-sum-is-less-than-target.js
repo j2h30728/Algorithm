ad6c7da6aa7134b688d395db559e212769e8f22f
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-07 21:36:18 +0900

    Time: 69 ms (21.63%), Space: 50.3 MB (76.22%) - LeetHub

--- LeetCode/Easy/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ LeetCode/Easy/2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -6,11 +6,14 @@
 var countPairs = function(nums, target) {
     nums.sort((a,b)=>a-b);
     let count = 0;
-    for(let i = 0; i < nums.length; i++){
-        for(let j =i+1; j < nums.length; j++){
-            if(nums[i] + nums[j] < target){
-                count++;
-            }
+    let start = 0;
+    let end = nums.length - 1;
+    while(start < end){
+        if(nums[start] + nums[end] < target){
+            count += end - start;
+            start++;
+        }else{
+            end--;
         }
     }
     return count;
