e3ee35556090e344e3582a9c870498c298575b90
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-12 08:51:58 +0900

    Time: 58 ms (31.32%), Space: 50.2 MB (17.39%) - LeetHub

---
+++ LeetCode/Easy/1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    const max = Math.max(word1.length, word2.length);
+    let result = '';
+    for (let i = 0; i < max; i++) {
+        if (word1[i]) result += word1[i]
+        if (word2[i]) result += word2[i]
+    }
+    return result;
+};
