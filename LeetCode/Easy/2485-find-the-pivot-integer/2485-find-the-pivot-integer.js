089869aed59983d3e8b79833fdd1ec2458169ece
Dam <60846068+j2h30728@users.noreply.github.com>
2024-03-14 09:10:29 +0900

    Time: 54 ms (71.80%), Space: 49.4 MB (23.08%) - LeetHub

--- LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
+++ LeetCode/Easy/2485-find-the-pivot-integer/2485-find-the-pivot-integer.js
@@ -2,9 +2,13 @@
  * @param {number} n
  * @return {number}
  */
+const obj = {};
 const sum = (num) => {
     if (num < 0) return 0;
-    return num + sum(num - 1);
+    if (!obj[num]) {
+        obj[num] = num + sum(num - 1);
+    }
+    return obj[num];
 }
 var pivotInteger = function (n) {
     for (let i = n; i >= 0; i--) {
