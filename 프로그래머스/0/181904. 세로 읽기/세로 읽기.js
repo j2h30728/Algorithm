c66815a1c33299c464fab37aeb6c70862c5cb904
Dam <60846068+j2h30728@users.noreply.github.com>
2024-05-29 12:07:30 +0900

    [level 0] Title: 세로 읽기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,7 +1,3 @@
 function solution(my_string, m, c) {
-    let result = ''
-    for(let i = 0; i < my_string.length ; i += m){
-        result += my_string.slice(i).at(c - 1);
-    }
-    return result;
+    return [...my_string].filter((x,i)=> i % m === (c -1) ).join('')
 }
