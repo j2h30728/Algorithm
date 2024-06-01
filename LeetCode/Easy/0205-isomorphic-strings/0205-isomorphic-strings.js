0245f33490e6e2fe54c8eaca0402fa6760173afd
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-01 22:40:02 +0900

    Time: 55 ms (85.11%), Space: 50.5 MB (59.86%) - LeetHub

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.js
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -4,8 +4,24 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    for(let i = 0; i < s.length; i++){
-        if(s.indexOf(s[i]) !== t.indexOf(t[i])) {
+    const sMap = new Map();
+    const tMap = new Map();
+
+    for(let i = 0 ; i < s.length; i ++){
+        const sChar = s[i];
+        const tChar = t[i];
+
+        if(!sMap.get(sChar)){
+            sMap.set(sChar, tChar);
+        }
+        if(sMap.get(sChar) !== tChar){
+            return false;
+        }
+
+        if(!tMap.get(tChar)){
+            tMap.set(tChar, sChar);
+        }
+        if(tMap.get(tChar) !== sChar){
             return false;
         }
     }
