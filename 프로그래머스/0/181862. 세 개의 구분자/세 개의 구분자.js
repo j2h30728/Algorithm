e95e128444d01056f765345644fb62ad29de20a2
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-02 22:08:28 +0900

    [level 0] Title: 세 개의 구분자, Time: 17.34 ms, Memory: 51 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,12 +1,4 @@
 function solution(myStr) {    
-    let string = ''
-    for(const str of myStr){
-        if(str === 'a' || str === 'b' || str === 'c' ){
-            string += ' '
-        }else {
-            string += str;
-        }
-    }
-    const result  = string.split(' ').filter(x=>x.length > 0);
+    const result  = myStr.split(/[a|b|c]/).filter(x=>x.length > 0);
     return result.length === 0 ? ["EMPTY"] : result;
 }
