7d5aa971477be549593a5f089ec7ebee48b494e8
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-03 22:26:10 +0900

    [level 0] Title: 2의 영역, Time: 0.30 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,12 +1,6 @@
 function solution(arr) {
-    if(!arr.includes(2)) return [-1];
-    
     const start = arr.indexOf(2);
     const end = arr.lastIndexOf(2);
     
-    if(end === -1){
-        return arr[start];
-    }else{
-        return arr.slice(start, end + 1);
-    }
+    return start === -1 ? [-1] : arr.slice(start, end + 1);
 }
