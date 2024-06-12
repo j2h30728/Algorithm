37547e5e66f8ffd9fe05514a7e6d10e23f74e9be
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-12 09:44:44 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,14 +1,8 @@
 function solution(str_list) {
-    const indexL = str_list.indexOf('l');
-    const indexR = str_list.indexOf('r');
-    if (indexL === -1 && indexR === -1) {
-        return [];
-    }
-    if (indexL !== -1 && (indexR === -1 || indexL < indexR)) {
-        return str_list.slice(0, indexL);
-    }
-    if (indexR !== -1 && (indexL === -1 || indexR < indexL)) {
-        return str_list.slice(indexR + 1);
-    }
+   for(let i = 0; i < str_list.length; i++){
+       const char = str_list[i];
+       if(char === 'l') return str_list.slice(0, i);
+       if(char === 'r') return str_list.slice(i + 1);
+   }
     return [];
 }
