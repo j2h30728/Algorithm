5d16bb3200fef9cd4e3b29ce889a428936a3b9f7
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-14 16:02:34 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.17 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,7 +1,6 @@
 function solution(arr, queries) {
-    return queries.map(query=>{
-        const [s, e, k] = query;
-        const filtered = arr.filter((x, i)=> (s <= i && i <= e) && x > k)
+    return queries.map(([s, e, k])=>{
+        const filtered = arr.slice(s, e + 1).filter((x)=> x > k)
         return filtered.length === 0 ? -1 : Math.min(...filtered);
         
     })
