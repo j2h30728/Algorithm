754f4746bd2e892fd473a65c65afcbdd342c74b1
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-15 23:45:32 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 6.40 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,7 +1,8 @@
 function solution(my_string, queries) {
-    queries.forEach(([s, e])=>{
-        const temp = my_string.slice(s, e + 1).split('').reverse().join('');
-        my_string = my_string.slice(0, s) + temp + my_string.slice(e + 1);
+    let arr = [...my_string]
+    queries.forEach(([start, end])=>{
+        const changeString = arr.slice(start, end + 1)
+        arr.splice(start, changeString.length , ...changeString.reverse())
     })
-    return my_string;
+    return arr.join('');
 }
