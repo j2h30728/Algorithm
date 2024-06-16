675da032aba8d7c36cc5e92f10b6ba17b614a5c2
Dam <60846068+j2h30728@users.noreply.github.com>
2024-06-16 19:31:12 +0900

    [level 0] Title: 그림 확대, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.js
@@ -1,16 +1,9 @@
 function solution(picture, k) {
     const result = [];
-    const rows = picture.map(x=>{
-        let temp = '';
-        for(const char of x){
-            temp += char.repeat(k);
-        }
-        return temp;
-    })
-    rows.forEach(x=>{
-        for(let i = 0; i < k; i++){
-            result.push(x);
-        }
-    })
+    picture.forEach(str=>{
+        const row = [...str].reduce((acc,cur)=>acc + cur.repeat(k), '')
+        
+        for(let i = 0; i < k; i++) result.push(row);
+    });
     return result;
 }
