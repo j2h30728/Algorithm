f4fd6be2ca5cc32c55fa1726fd4ed83d2bc58f5c
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-20 16:28:46 +0900

    [level 0] Title: 모스부호 (1), Time: 0.07 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,13 @@
+const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+};
+
+function solution(letter) {
+    const formatLetter = letter.split(' ');
+    return formatLetter.map(letter => morse[letter]).join('');
+    
+}
