8753564ceabb0c4483dd5a32d6b7455fe9ac8b83
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-23 22:25:38 +0900

    [level 0] Title: 소인수분해, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -1,29 +1,11 @@
-const isPrime = (num) => {
-    if(num === 2) return true;
-    
-    for(let i = 2; i <= Math.sqrt(num); i++){
-        if(num % i === 0) return false;
-    }
-    return true;
-}
-
-const calPrime = (num) => {
-    const arr = [];
-    let count = 2;
-    while(count <= num){
-        if(num % count === 0){
-            num = Math.floor(num / count);
-            if(isPrime(count)){
-                arr.push(count);   
-            }
-        }
-        count++;
-    }
-    return arr;
-}
-
 function solution(n) {
-    const set = new Set(calPrime(n))
-    return [...set].sort((a,b) => a - b);
+    const result = [];
+    for(let i = 2; i <= n; i++){
+        while(n % i === 0){
+            n = Math.floor(n / i);
+            result.push(i);
+        }
+    }
+    return [...new Set(result)];
 }
 
