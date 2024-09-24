c3f72425c703536e0b3568a247f23049cb3a0712
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-24 20:27:08 +0900

    [level 1] Title: 모의고사, Time: 3.06 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,8 +1,20 @@
 function solution(answers) {
-    const paterns = [ [1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
-    const result = paterns.map((patern,index) => {
-        return answers.filter((x,i) => patern[i % patern.length] === x).length;
-    });
-    const max = Math.max(...result);
-    return result.map((x,i) => [x,i + 1]).filter(x => x[0] === max).sort((a, b)=> a[0] - b[0]).map(x => x[1]);
+    const paterns = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
+    
+    const scores = [0, 0, 0];
+    answers.map((answer, i)=>{
+        paterns.map((patern, j)=>{
+            if(answer === patern[i % patern.length]){
+                scores[j] += 1;
+            }
+        })
+    })
+    const max = Math.max(...scores);
+    const result = [];
+    for(let i = 0; i < scores.length; i++){
+        if(max === scores[i]){
+            result.push(i + 1);
+        }
+    }
+    return result;
 }
