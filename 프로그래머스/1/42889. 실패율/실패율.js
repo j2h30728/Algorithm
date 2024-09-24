c4c2de3b1e1b7d3a7ab18e57c0500dac4315d157
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-25 01:23:10 +0900

    [level 1] Title: 실패율, Time: 146.13 ms, Memory: 39.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,17 @@
+function solution(N, stages) {
+    const success = new Array(N + 1).fill(0);
+    stages.forEach((stage) => {
+        for(let i = 0; i < stage; i++){
+            success[i] += 1;
+        }
+    })
+    const arr = [];
+    let people = Math.max(...success);
+    for(let i = 0; i < success.length - 1; i++){
+        const failCount = success[i] - success[i + 1]
+        arr.push(failCount / people);
+        people -= failCount;
+    }
+    const result = Object.entries(arr).sort((a,b)=> b[1] - a[1]).map(x => +x[0] + 1);
+    return result;
+}
