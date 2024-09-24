72464571d8c343ff6fcd2c7ca2d54fbf1214bdea
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-24 20:54:40 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.32 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,11 +1,17 @@
 function solution(arr1, arr2) {
-    const rowsArr1 = arr1.length;
-    const colsArr1 = arr1[0].length;
-    const colsArr2 = arr2[0].length;
-    const result = Array.from({ length: rowsArr1 }, () => Array(colsArr2).fill(0));
-    for(let i = 0; i < rowsArr1; i++){
-        for(let j = 0; j < colsArr2; j++){
-            for(let k = 0; k < colsArr1; k++){
+    const row1 = arr1.length; // 3
+    const col1 = arr1[0].length; // 2
+    
+    const row2 = arr2.length; // 2
+    const col2 = arr2[0].length; // 2
+    
+    // 2 * 3 (가로 2 , 세로 3)
+    const result = [...new Array(row1)].map((_, i)=> new Array(col2).fill(0));
+    
+    
+    for(let i = 0; i < row1; i++){
+        for(let j = 0; j < col2; j++){
+            for(let k = 0; k < col1; k++){
                 result[i][j] += arr1[i][k] * arr2[k][j];
             }
         }
