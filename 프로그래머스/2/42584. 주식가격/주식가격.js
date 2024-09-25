211ed8fc9cbd89518ccef954bfe8f115a5b9459a
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-25 16:43:06 +0900

    [level 2] Title: 주식가격, Time: 37.74 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,10 +1,17 @@
 function solution(prices) {
-    var answer = new Array(prices.length).fill(0);
-    for(let i = 0; i < prices.length -1; i++){
-        for(let j = i + 1; j < prices.length; j++){
-            answer[i]++;
-            if(prices[i] > prices[j]) break;
+    const length = prices.length;
+    const answer = new Array(length).fill(0);
+    
+    const stack = [0];
+    
+    for(let i = 1; i < length; i++){
+        while(stack.length > 0 && prices[stack.at(-1)] > prices[i] ){
+            const j = stack.pop();
+            answer[j] = i - j;
         }
+        stack.push(i);
+        
     }
+    stack.forEach(x => answer[x] = length - x - 1)
     return answer;
 }
