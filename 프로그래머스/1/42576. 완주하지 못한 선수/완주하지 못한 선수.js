4f6b4acac8a9bdf06a28ae3f388d6029aa4e2cdf
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-27 17:16:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.27 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,11 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-    for(let i = 0; i < participant.length; i++){
-        if(participant[i] !== completion[i]) return participant[i];
+    const map = new Map();
+    completion.forEach(x => map.set(x, (map.get(x) || 0) + 1));
+    for(const name of participant){
+        if(!map.get(name)){
+            return name;
+        }else{
+            map.set(name, map.get(name) - 1);
+        }
     }
 }
