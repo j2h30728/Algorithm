df444277d380fb7ea14d925de41ebee2c11698a7
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-29 01:21:14 +0900

    [level 2] Title: 전화번호 목록, Time: 154.51 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,8 @@
 function solution(phone_book) {
-    const map = new Map()
-    phone_book.forEach((number)=> map.set(number,true))
-    
-    for(const phone of phone_book){
-        for(let i = 0; i < phone.length; i++){
-            const current = phone.slice(0,i);
-            if(map.has(current)) return false;
+    phone_book.sort();
+    for(let i = 0; i < phone_book.length; i++){
+        if(String(phone_book[i + 1]).startsWith(phone_book[i])){
+            return false;
         }
     }
     return true;
