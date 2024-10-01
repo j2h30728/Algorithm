58535ee6c916ac4e16c4bd0fe830b7f7bed01a57
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-01 16:29:39 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,25 +1,21 @@
-function solution(keyinput, board) {
-    const move = {
-        up : [0, 1],
-        down : [0 , -1],
-        left : [-1, 0],
-        right : [1 ,0]
-    }
-    const current = [0, 0];
-    
-    keyinput.forEach((dir) => {
-        const moving = move[dir];
-        if(isValidMoving(board[0], current[0] + moving[0])){
-            current[0] += moving[0];
-        }
-        if(isValidMoving(board[1], current[1] + moving[1])) {
-            current[1] += moving[1];
-        }
-    })
-    return current;
+const move = {
+    up : [0, 1],
+    down : [0 , -1],
+    left : [-1, 0],
+    right : [1 ,0]
 }
 
+function solution(keyinput, board) {
+    const [x1, x2] = [-board[0] / 2, board[0] / 2];
+    const [y1, y2] = [-board[1] / 2, board[1] / 2];
+    const current = [0, 0];
+    
+    return keyinput.reduce(([x , y], cur) => {
+        const [nx, ny] = [x + move[cur][0], y + move[cur][1]]
+        if(nx >= x1 && nx <= x2 && ny >= y1 && ny <= y2 ){
+            return [nx, ny];
+        }
+        return [x, y];
+    },[0,0])
 
-function isValidMoving (boardValue, calculateValue) {
-    return Math.abs(calculateValue) <= Math.floor(boardValue / 2);
 }
