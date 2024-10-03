188394b2b326a8a6d42f3d1f70badda22b1e34f2
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-03 23:41:23 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.39 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,13 +1,4 @@
 function solution(lines) {
-    const start = Math.min(...lines.flat());
-    const end = Math.max(...lines.flat());
-
-    const arr = Array.from({length: end - start}, () => 0);
-    
-    lines.forEach(([s, e]) => {
-        for(let i = s; i < e; i++){
-            arr[i - start] += 1;
-        }
-    })
-    return arr.filter(x => x > 1).length;
+    var answer = 0;
+    return answer;
 }
