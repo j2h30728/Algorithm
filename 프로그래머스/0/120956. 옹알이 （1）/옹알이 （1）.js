6e683409d1c8f1061f0e905589229bc1d2e8dd14
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-04 23:55:18 +0900

    [level 0] Title: 옹알이 (1), Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,4 +1,18 @@
 function solution(babbling) {
-    var answer = 0;
-    return answer;
+    const set = new Set([ "aya", "ye", "woo", "ma" ]);
+    let count = 0;
+    for(const string of babbling){
+        let pre = '';
+        let store = '';
+        for(const char of string){
+            pre += char;
+            
+            if(set.has(pre)){
+                store += pre;
+                pre = '';
+            }
+        }
+        if(string === store) count++;
+    }
+    return count;
 }
