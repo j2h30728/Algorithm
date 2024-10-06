b861ed3859023f39476909e3d81bf191678b7c2c
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-06 17:12:44 +0900

    [level 0] Title: 문자열 밀기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,10 +1,4 @@
 function solution(A, B) {
-    const newA = A + A;
-    for(let i = A.length; i > 0; i--){
-        const a = newA.slice(i , i + A.length);
-        if(a === B){
-            return A.length - i;
-        }
-    }
-    return -1;
+    const newB = B + B;
+    return newB.indexOf(A);
 }
