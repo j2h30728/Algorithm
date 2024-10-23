8e60a0d95337e89d6fc69d35f902441451a0ed90
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-23 19:24:17 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 5.23 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,9 +1,4 @@
 function solution(n) {
-    let num = 0;
-    while(num < n){
-        if(n % num === 1){
-            return num;
-        } 
-        num++;
-    }
+    var answer = 0;
+    return answer;
 }
