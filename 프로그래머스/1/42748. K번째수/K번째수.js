63c4ad5c65482b85233237fe1c5913cf4395b30d
Dam <60846068+j2h30728@users.noreply.github.com>
2024-11-15 08:43:33 +0900

    [level 1] Title: K번째수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,10 +1,3 @@
 function solution(array, commands) {
-    const result = [];
-    for(let index = 0; index < commands.length; index++){
-        const [i,j,k] = commands[index]
-        const slicedArray = array.slice(i - 1, j ).sort((a,b)=> a - b);
-        
-        result.push(slicedArray[k - 1]);
-    }
-    return result;
+    return commands.map(([start, end, index]) => array.slice(start - 1, end).sort((a, b) => a - b).at(index - 1));
 }
