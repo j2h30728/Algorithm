45121f705f96a16aa19bea168371d30dc9c07c23
Dam <60846068+j2h30728@users.noreply.github.com>
2024-11-21 09:13:34 +0900

    [level 1] Title: 소수 찾기, Time: 103.50 ms, Memory: 44.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    const isPrime = Array.from({length : n + 1}).fill(true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    
+    for(let i = 2; i * i <= n; i++){
+        if(isPrime[i]){
+            for(let j = i * i; j <= n; j += i){
+                isPrime[j] = false;
+            }
+        }
+    }
+    return isPrime.filter(Boolean).length;
+}
