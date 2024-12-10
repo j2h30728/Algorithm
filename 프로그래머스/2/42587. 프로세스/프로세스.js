aa0b41425f13bbe0513d9ba3b8a55acf7321dcd6
Dam <60846068+j2h30728@users.noreply.github.com>
2024-12-10 20:01:27 +0900

    [level 2] Title: 프로세스, Time: 0.68 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,17 +1,18 @@
 function solution(priorities, location) {
-    const arr = priorities.map((priority, index) => ({priority, index}));
+    const arr = priorities.map((x, i) => ({ value : x, index : i}));
     let count = 0;
-
-    while (arr.length > 0) {
-        const current = arr.shift();
-        if (arr.some(item => item.priority > current.priority)) {
-            arr.push(current);
-        } else {
+    while(arr.length > 0) {
+        if(arr.slice(1).some(x => arr[0].value < x.value)){
+            const temp = arr.shift()
+            arr.push(temp);
+        }else{
+            const temp = arr.shift();
             count++;
-            if (current.index === location) {
+            if(temp.index === location){
                 return count;
             }
         }
+        
     }
     return count;
 }
