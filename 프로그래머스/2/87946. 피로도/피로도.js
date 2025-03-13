bdc8285f1604f56e0cc56a9d8472ba39bb756c23
Dam <60846068+j2h30728@users.noreply.github.com>
2025-03-13 16:33:30 +0900

    [level 2] Title: 피로도, Time: 14.11 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,21 +1,4 @@
 function solution(k, dungeons) {
-    const n = dungeons.length
-    const visited = (new Array(n)).fill(false);
-    let maxCount = 0;
-    
-    const backTracking = (count, fatigue) => {
-        maxCount = Math.max(count, maxCount);
-        
-        for(let i = 0; i < n; i++){
-            const [need, using] = dungeons[i]
-            if(!visited[i] && fatigue >= need){
-                visited[i] = true;
-                backTracking(count + 1, fatigue - using);
-                visited[i] = false;
-            }
-        }
-    }
-    backTracking(0, k);
-    
-    return maxCount;
+    var answer = -1;
+    return answer;
 }
