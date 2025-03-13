88404b27608ad5f0809de3a44c4f644139492f8d
Dam <60846068+j2h30728@users.noreply.github.com>
2025-03-13 21:55:00 +0900

    [level 2] Title: 카펫, Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,18 @@
 function solution(brown, yellow) {
-    let yellowCol = 1;
-    while(true){
-        let devidedYellow = yellow / yellowCol;
-        let calBrown = (devidedYellow + 2) * 2 + yellowCol * 2;
-        if(calBrown === brown){
-            return [devidedYellow+2, yellowCol+2]
+    // 가로  >= 세로
+    const total = brown + yellow;
+    
+    for(let i = 1; i <= Math.sqrt(yellow); i++){
+        if(yellow % i === 0){
+            const yellowWidth = yellow / i;
+            const yellowHeight = i;
+            
+            const width = yellowWidth + 2;
+            const height = yellowHeight + 2;
+            
+            if(width * height === total){
+                return [width, height];
+            }
         }
-        yellowCol++;
     }
 }
