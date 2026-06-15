09ec8ce68f7eda01e45fd7effe91efeb581b3cf7
Dahm <60846068+j2h30728@users.noreply.github.com>
2026-06-15 20:56:47 +0900

    [Passed] 2021년 날짜의 유무 (66ms, 10MB)

--- codetree/trail2/2021년 날짜의 유무/with-or-without-2021.js
+++ codetree/trail2/2021년 날짜의 유무/with-or-without-2021.js
@@ -3,22 +3,24 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 let [m, d] = input[0].split(" ").map(Number);
 
 // Please Write your code here.
-const MONTHS_30 = [4, 6, 9, 11];
-
-function isValid(m, d) {
-    if (m > 12){
-        return false;
-    }
-
+function lastDayNumber(m) {
     if (m === 2) {
-        return d <= 28;
+        return 28;
     }
 
-    if (MONTHS_30.includes(m)) {
-        return d <= 30;
+    if (m === 4 || m === 6 || m === 9 || m === 11) {
+        return 30;
     }
 
-    return d <= 31;
+    return 31;
 }
 
-console.log(isValid(m, d) ? 'Yes' : 'No')
+function judgeDay(m, d){
+    if(m <= 12 && d <= lastDayNumber(m)){
+        return true;
+    }
+
+    return false
+}
+
+console.log(judgeDay(m, d) ? 'Yes' : 'No')
