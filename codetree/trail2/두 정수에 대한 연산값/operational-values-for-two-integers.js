9f82474e779a4df5bf779025a2f14efeb044561b
Dahm <60846068+j2h30728@users.noreply.github.com>
2026-06-15 21:49:09 +0900

    [Passed] 두 정수에 대한 연산값 (62ms, 10MB)

--- codetree/trail2/두 정수에 대한 연산값/operational-values-for-two-integers.js
+++ codetree/trail2/두 정수에 대한 연산값/operational-values-for-two-integers.js
@@ -3,12 +3,18 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 let [a, b] = input[0].split(" ").map(Number);
 // Please Write your code here.
 
-function cal(a, b){
+function changeNumber(a, b){
     if(a > b){
-        console.log((a + 25) + ' ' + b * 2);
-    } else{
-        console.log(a * 2 + ' ' + (b + 25));
+        b *= 2;
+        a += 25;
+    }else{
+        a *= 2;
+        b += 25;
     }
+
+    return [a ,b];
 }
 
-cal(a,b);
+[a, b] = changeNumber(a, b);
+
+console.log(a, b);
